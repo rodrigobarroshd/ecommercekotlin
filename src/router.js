812c243e1b1fcfe29aa9394b586854c7ebfcd2a0
Rodrigo Barros <brodrigo87@gmail.com>
2022-12-13 12:50:02 -0300

    router

--- src/router.js
+++ src/router.js
@@ -2,3 +2,32 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
  
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const Stack = createNativeStackNavigator();
+
+function Routes(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}        
+                />
+
+                <Stack.Screen
+                name="Detail"
+                component={Detail}
+                options={{ headerShown: false }}
+   
+                />
+                 
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;
